a6555f845006ebbc64973212ea276c9c42b619a8
Xavi <xavi@geekshubs.com>
2019-05-14 11:55:31 +0200

    added di container and up

--- src/kernel.php
+++ src/kernel.php
@@ -3,15 +3,21 @@ namespace App;
 
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
+use DI\ContainerBuilder;
+use Kint;
 
 class kernel
 {
     public $logger;
+    private $container;
+
 
     public function __construct()
     {
         $this->logger = new Logger('Kernel');
         $this->logger->pushHandler(new StreamHandler(dirname(__DIR__).'/var/log/prod.log'), Logger::DEBUG);
+        $this->container = $this->createContainer();
+       // Kint::dump($this->container);
     }
 
     public function init()
@@ -19,5 +25,20 @@ class kernel
        
         $this->logger->info('Kernel up');
         echo "<h1>Arrancado kernel</h1>";
+    }
+
+
+    //create container builder
+    public function createContainer()
+    {
+      //Create instance
+      $containerBuilder = new ContainerBuilder();
+
+      //Autowiring true 
+      $containerBuilder->useAutowiring(true);
+
+      //Create container
+      return $containerBuilder->build();
+
     }
 }
